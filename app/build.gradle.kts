a60a037d1555a34793209b5c60bdd29b3a6f61a7
droid-lover <ersachin20@gmail.com>
2024-11-19 23:33:05 +0530

    Added dependencies for di & coroutines

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt.android)
+    kotlin("kapt")
+
 }
 
 android {
@@ -30,11 +33,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
     buildFeatures {
         compose = true
@@ -66,4 +69,12 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.coroutines.android)
+
+    implementation(libs.hilt)
+    kapt(libs.hilt.compiler)
+    //Hilt Navigation Compose
+    implementation(libs.hilt.navigation.compose)
+
 }
--- app/src/main/java/com/yuna/selfgrowthapp/common/SelfGrowthApplication.kt
+++ app/src/main/java/com/yuna/selfgrowthapp/common/SelfGrowthApplication.kt
@@ -2,7 +2,9 @@ package com.yuna.selfgrowthapp.common
 
 import android.app.Application
 import android.util.Log
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class SelfGrowthApplication : Application() {
 
     override fun onCreate() {
