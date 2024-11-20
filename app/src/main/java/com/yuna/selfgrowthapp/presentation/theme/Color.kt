ed0b924aa07b13d7865ebf11d6391d0dafb324f0
droid-lover <ersachin20@gmail.com>
2024-11-21 03:18:08 +0530

    Added colors and drawables

--- app/src/main/java/com/yuna/selfgrowthapp/presentation/theme/Color.kt
+++ app/src/main/java/com/yuna/selfgrowthapp/presentation/theme/Color.kt
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val greenColor = Color(0xFF66BA24)
+val greenTextColor = Color(0x4066BA24)
+val normalTextColor = Color(0xFF1E2220)
+val normalTextColor70 = Color(0x701E2220)
+val bgColor = Color(0xf8f6f6FF)
